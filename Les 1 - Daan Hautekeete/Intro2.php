ec0fecaad57d513e695a7054f9932949e17bc2d6
Daan Hautekeete <Hautekeete.Daan@gmail.com>
2025-01-21 10:11:13 +0100

    werken aan opdracht

---
+++ Les 1 - Daan Hautekeete/Intro2.php
@@ -0,0 +1,81 @@
+<?php
+  $leerlingen=array("Lobke Bonte_6BI"=>"BonteLobke","Simon Declerck_6BI"=>"DeclerckSimon","Emma Devolder_6BI"=>"DevolderEmma","Alexander Dossche_6BI"=>"DosscheAlexander","Daan Hautekeete_6BI"=>"HautekeeteDaan","Aaron Injon_6BI"=>"InjonAaron","Pahul Malhi_6BI"=>"MalhiPahul","Tuur Mispelaere_6BI"=>"MispelaereTuur","Stan Tanghe_6BI"=>"TangheStan","Luka Verbrugghe_6BI"=>"VerbruggheLuka", "Milo Claessens Pillen_6AD"=>"ClaessensPillenMilo","Celestin De Marez_6AD"=>"DeMarezCelestin","Senna Devolder_6AD"=>"DevolderSenna","Gilles Longueville_6AD"=>"LonguevilleGilles","Hugo Popelier_6AD"=>"PopelierHugo","Joshua Radford_6AD"=>"RadfordJoshua","Arthur Roets_6AD"=>"RoetsArthur","Julian Stoops_6AD"=>"StoopsJulian","Maxim Van Belle_6AD"=>"VanBelleMaxim","Bouke Vandenbussche_6AD"=>"VandenbusscheBouke","Sem Van Eenoge_6AD"=>"VanEenogeSem");
+
+  //var maken als er een leerling gekozen is
+  $leerlingGekozen = false;
+
+
+  //als er een leerling gekozen is
+  if(isset($_POST['cboLeerlingen'])) {
+    $gekozenLeerling = $_POST['cboLeerlingen'];
+    $gekozenFoto = $leerlingen[$gekozenLeerling];
+    $output = "<p>Gekozen leerling $gekozenLeerling.</p><br><img src='fotosSJZ/$gekozenFoto.jpg' alt='foto van $gekozenLeerling'>";
+    $leerlingGekozen = true;
+  }
+
+  //array in de lijst steken
+  foreach($leerlingen as $leerling=>$foto) {
+    //naam uit de leerling halen - strpos = positie van de underscore
+    $leerlingNaam = substr($leerling, 0, strpos($leerling, "_"));
+    $combo .= "<option";
+    //kijken als de leerling de gekozen leerling is
+    if($leerling == $gekozenLeerling) {
+      $combo .= " selected";
+    }
+    $combo .= ">$leerlingNaam</option>\n";
+  }
+
+
+?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>PHP basisoefeningen</title>
+<style type="text/css">
+	body {
+	color: #0A0A55;		
+	}
+	#wrapper {
+  width: 1080px;
+	margin-left: auto;
+	margin-right: auto;
+	border: solid 1px #0070C0;
+	padding: 0.3em;
+	}	
+	main {
+	min-height: 20em;
+	padding: 1em;
+	}
+	footer {
+	height: 0.5em;
+	background-color: #0070C0;
+	}
+</style>
+</head>
+
+<body>
+<div id="wrapper">
+	<header><img src="images/banner.jpg" width="100%"  alt=""/></header>
+	<main>
+    <form name="frmLeerling" method="Post">
+
+      <select name="cboLeerlingen" onchange="document.frmLeerling.submit()">
+        <option>Kies een leerling</option>
+        <?php
+        echo $combo;
+        ?>
+      </select>
+    </form>
+
+    <?php
+    if($leerlingGekozen) {
+      echo $output;
+    }
+    ?>
+  </main>
+	<footer>&nbsp;</footer>
+</div>
+</body>
+</html>
