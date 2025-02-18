0337f953cef25d19c9a86528f024e885e7c787bd
Daan Hautekeete <Hautekeete.Daan@gmail.com>
2025-02-18 10:02:58 +0100

    afwerken oefening fotolijst (variant 1)

--- Les 1 - Daan Hautekeete/basis_oef10SJZ_fotolijst.php
+++ Les 1 - Daan Hautekeete/basis_oef10SJZ_fotolijst.php
@@ -1,3 +1,4 @@
+
 <!doctype html>
 <html>
 <head>
@@ -8,7 +9,7 @@
 	color: #0A0A55;		
 	}
 	#wrapper {
-width: 1080px;
+	width: 1080px;
 	margin-left: auto;
 	margin-right: auto;
 	border: solid 1px #0070C0;
@@ -21,20 +22,21 @@ width: 1080px;
 	footer {
 	height: 0.5em;
 	background-color: #0070C0;
+	clear: both;
 	}
-    #persoon {
-float: left;	
-margin: 0.5em;
-border: solid 1px #0AC;
-text-align: center;
-}
-#naam {
-color: #FFF;
-background-color: #0AC;	
-}
-.startonder {
-clear: left;	
-}
+  .persoon {
+		float: left;	
+		margin: 0.5em;
+		border: solid 1px #0AC;
+		text-align: center;
+	}
+	.naam {
+		color: #FFF;
+		background-color: #0AC;	
+	}
+	.startonder {
+		clear: left;	
+	}
 </style>
 </head>
 
@@ -43,8 +45,17 @@ clear: left;
 	<header><img src="images/banner.jpg" width="100%"  alt=""/></header>
 	<main>
     <?php
-$leerlingen2=array("Lobke Bonte"=>"BonteLobke","Simon Declerck"=>"DeclerckSimon","Emma Devolder"=>"DevolderEmma","Alexander Dossche"=>"DosscheAlexander","Daan Hautekeete"=>"HautekeeteDaan","Aaron Injon"=>"InjonAaron","Pahul Malhi"=>"MalhiPahul","Tuur Mispelaere"=>"MispelaereTuur","Stan Tanghe"=>"TangheStan","Luka Verbrugghe"=>"VerbruggheLuka");
-$leerlingen=array("Milo Claessens Pillen"=>"ClaessensPillenMilo","Celestin De Marez"=>"DeMarezCelestin","Senna Devolder"=>"DevolderSenna","Gilles Longueville"=>"LonguevilleGilles","Hugo Popelier"=>"PopelierHugo","Joshua Radford"=>"RadfordJoshua","Arthur Roets"=>"RoetsArthur","Julian Stoops"=>"StoopsJulian","Maxim Van Belle"=>"VanBelleMaxim","Bouke Vandenbussche"=>"VandenbusscheBouke","Sem Van Eenoge"=>"VanEenogeSem");
+			$leerlingen=array("Lobke Bonte"=>"BonteLobke","Simon Declerck"=>"DeclerckSimon","Emma Devolder"=>"DevolderEmma","Alexander Dossche"=>"DosscheAlexander","Daan Hautekeete"=>"HautekeeteDaan","Aaron Injon"=>"InjonAaron","Pahul Malhi"=>"MalhiPahul","Tuur Mispelaere"=>"MispelaereTuur","Stan Tanghe"=>"TangheStan","Luka Verbrugghe"=>"VerbruggheLuka", "Milo Claessens Pillen"=>"ClaessensPillenMilo","Celestin De Marez"=>"DeMarezCelestin","Senna Devolder"=>"DevolderSenna","Gilles Longueville"=>"LonguevilleGilles","Hugo Popelier"=>"PopelierHugo","Joshua Radford"=>"RadfordJoshua","Arthur Roets"=>"RoetsArthur","Julian Stoops"=>"StoopsJulian","Maxim Van Belle"=>"VanBelleMaxim","Bouke Vandenbussche"=>"VandenbusscheBouke","Sem Van Eenoge"=>"VanEenogeSem");
+
+			
+			foreach($leerlingen as $leerling=>$foto) {
+				$output .= "<div class='persoon'>";
+				$output .= "<img src='fotosSJZ/$foto.jpg' alt='foto van $leerling'> <div class='naam'><p>$leerling</p></div>";
+				$output .= "</div>";
+			}
+
+
+			echo $output;
     ?>
     
     </main>
