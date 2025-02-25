229f7bd13c8e42a112654400da6d3db787952f82
Daan Hautekeete <Hautekeete.Daan@gmail.com>
2025-02-25 09:16:42 +0100

    Oefening fotolijst met klaskeuze afwerken

--- Les 1 - Daan Hautekeete/basis_oef10SJZ_fotolijst - met klaskeuze.php
+++ Les 1 - Daan Hautekeete/basis_oef10SJZ_fotolijst - met klaskeuze.php
@@ -1,7 +1,23 @@
 <?php
-$leerlingen=array("Lobke Bonte_6BI"=>"BonteLobke","Simon Declerck_6BI"=>"DeclerckSimon","Emma Devolder_6BI"=>"DevolderEmma","Alexander Dossche_6BI"=>"DosscheAlexander","Daan Hautekeete_6BI"=>"HautekeeteDaan","Aaron Injon_6BI"=>"InjonAaron","Pahul Malhi_6BI"=>"MalhiPahul","Tuur Mispelaere_6BI"=>"MispelaereTuur","Stan Tanghe_6BI"=>"TangheStan","Luka Verbrugghe_6BI"=>"VerbruggheLuka", "Milo Claessens Pillen_6AD"=>"ClaessensPillenMilo","Celestin De Marez_6AD"=>"DeMarezCelestin","Senna Devolder_6AD"=>"DevolderSenna","Gilles Longueville_6AD"=>"LonguevilleGilles","Hugo Popelier_6AD"=>"PopelierHugo","Joshua Radford_6AD"=>"RadfordJoshua","Arthur Roets_6AD"=>"RoetsArthur","Julian Stoops_6AD"=>"StoopsJulian","Maxim Van Belle_6AD"=>"VanBelleMaxim","Bouke Vandenbussche_6AD"=>"VandenbusscheBouke","Sem Van Eenoge_6AD"=>"VanEenogeSem");
+	$leerlingen=array("Lobke Bonte_6BI"=>"BonteLobke","Simon Declerck_6BI"=>"DeclerckSimon","Emma Devolder_6BI"=>"DevolderEmma","Alexander Dossche_6BI"=>"DosscheAlexander","Daan Hautekeete_6BI"=>"HautekeeteDaan","Aaron Injon_6BI"=>"InjonAaron","Pahul Malhi_6BI"=>"MalhiPahul","Tuur Mispelaere_6BI"=>"MispelaereTuur","Stan Tanghe_6BI"=>"TangheStan","Luka Verbrugghe_6BI"=>"VerbruggheLuka", "Milo Claessens Pillen_6AD"=>"ClaessensPillenMilo","Celestin De Marez_6AD"=>"DeMarezCelestin","Senna Devolder_6AD"=>"DevolderSenna","Gilles Longueville_6AD"=>"LonguevilleGilles","Hugo Popelier_6AD"=>"PopelierHugo","Joshua Radford_6AD"=>"RadfordJoshua","Arthur Roets_6AD"=>"RoetsArthur","Julian Stoops_6AD"=>"StoopsJulian","Maxim Van Belle_6AD"=>"VanBelleMaxim","Bouke Vandenbussche_6AD"=>"VandenbusscheBouke","Sem Van Eenoge_6AD"=>"VanEenogeSem");
 
+	$show = "form";
+	if(isset($_POST["btnKlas"])) 
+	{
+		$show = "output";
+		$gekozenklas = $_POST["rdbKeuzeKlas"];
 
+		foreach($leerlingen as $leerling=>$foto) {
+			$klasVanLeerling = substr($leerling, -3);
+			$naamVanLeerling = substr($leerling, 0, strpos($leerling, "_"));
+			
+			if($klasVanLeerling == $gekozenklas) {				
+				$output .= "<div class='persoon'>";
+				$output .= "<img src='fotosSJZ/$foto.jpg' alt='foto van $leerling'> <div class='naam'><p>$naamVanLeerling</p></div>";
+				$output .= "</div>";
+			}
+		}
+	}
 ?>
 <!doctype html>
 <html>
@@ -31,14 +47,14 @@ width: 960px;
 	clear: both;
 	}
 	
-#persoon {
+.persoon {
 float: left;	
 margin: 0.5em;
 border: solid 1px #DD0B2F;
 text-align: center;
 width: 215px;
 }
-#naam {
+.naam {
 color: #FFF;
 background-color: #DD0B2F;	
 padding: 0.5em;
@@ -59,12 +75,24 @@ margin-bottom:2em;    }
 <div id="wrapper">
 	<header><img src="images/banner.jpg" width='100%' alt=""/></header>
 <div id="content">
+	<?php
+	if($show == "form")  {
+		
+	?>
 	<form name="frmKlas" method="post">
-	<input type="radio" name="rdb6AD" value = "6AD" id="6AD" checked> <label for="6AD">6AD</label>
-	<input type="radio" name="rdb6BI" value = "6BI"> <label for="6BI">6BI</label>
-	<input type="submit" name="btnKlas" value="Kies klas">
-	</form>    
+		<input type="radio" name="rdbKeuzeKlas" value = "6AD" id="6AD" checked> <label for="6AD">6AD</label>
+		<input type="radio" name="rdbKeuzeKlas" id="6BI" value = "6BI"> <label for="6BI">6BI</label>
+		<input type="submit" name="btnKlas" value="Kies klas">
+	</form>
+	<?php
+		}
+
+	elseif($show == "output") {
+		echo $output;
+	}
+	?>
 </div>
+
 <footer>&nbsp;</footer>
 </div>
 </body>
