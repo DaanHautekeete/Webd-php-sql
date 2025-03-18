ee220d6c5955f6e66d05938bac91a8d25305cd8a
Daan Hautekeete <Hautekeete.Daan@gmail.com>
2025-03-18 10:09:23 +0100

    keuzelijst vullen met provincies

--- Databanken/dbles3_opg/postcodes.php
+++ Databanken/dbles3_opg/postcodes.php
@@ -3,10 +3,21 @@
   include("cnndbles3.php");
 
   $sql = "select postcode,gemeente,deelgemeente from tblpostcodes order by postcode";
+  $sqlProvincies = "select DISTINCT provincie from tblpostcodes ";
 
   // $db => uit cnndbles3
   // Die => foutopvang
   $result = $db->query($sql) or die("Ophalen data mislukt!");
+  $resultProvincies = $db->query($sqlProvincies) or die("Ophalen data mislukt!");
+
+  // Keuzelijst vullen met provincies
+  while($provincies = $resultProvincies->fetch_assoc()) {
+    $provincie = $provincies['provincie'];
+
+    $vulLijstProvincies .= "<option>$provincie</option>";
+  }
+
+
 
   $teller = 1;
   $blokje = 0;
@@ -93,9 +104,9 @@ text-align: right;
 <tr><td>Leeftijd</td><td><input name="txtLeeftijd" type="number"  /></td></tr>
 <tr><td>Keuze</td><td>
 <select name="cboKeuze">
-<option value="1">Keuze 1</option>    
-<option value="2">Keuze 2</option>    
-<option value="3">Keuze 3</option>        
+<?php
+  echo $vulLijstProvincies;
+?>
 </select>
     </td></tr>    
 <tr><td>&nbsp;</td><td><input type="submit" name="btnVersturen" value="Verstuur" /></td></tr>    
