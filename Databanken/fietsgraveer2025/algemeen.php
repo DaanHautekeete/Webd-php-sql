1454c36aed8ef9bdfb5e7a80823dd585024dbf80
Daan Hautekeete <Hautekeete.Daan@gmail.com>
2025-05-06 10:09:12 +0200

    WIP inloggen en uitloggen

--- Databanken/fietsgraveer2025/algemeen.php
+++ Databanken/fietsgraveer2025/algemeen.php
@@ -1,4 +1,55 @@
 <?php
 session_start();
-$_SESSION['inlogstatus']="ingelogd";
+
+// In- en uitloggen (uitloggen moet zeker conditioneel zijn)
+// Uitloggen:
+if(isset($_POST["btnLogout"]))
+{
+  $_SESSION['login'] = "";
+  $_SESSION['vnaam'] = "";
+  $_SESSION['fnaam'] = "";
+  $_SESSION['regid'] = "";
+  $_SESSION['weergave'] = "inloggen";
+}
+
+// Inloggen
+if($_POST['btnLogin'] == 'Login') 
+{
+  $vnaam = $_POST['txtVnaam'];
+  $fnaam = $_POST['txtFnaam'];
+  $wachtwoord = $_POST['txtWW'];
+
+  $sqlLogin = "SELECT * from tblregistratie WHERE voornaam='$vnaam' AND fnaam='$fnaam'";
+  $resultLogin = $db->query($sqlLogin);
+  $rowLogin = $resultLogin->fetch_assoc();
+
+  // num_rows is een eigenschap dat het aantal rijen telt in dataset
+  if($resultLogin->num_rows == 0) 
+  {
+    $loginFout = "Gebruiker onbekend!";
+  }
+  else 
+  {
+    if($rowLogin['wachtwoord'] == $wachtwoord) 
+    {
+      // Het inloggen is oké
+      $loginFout = "Correct ingelogd!";
+
+      $_SESSION['login'] = "ingelogd";
+      $_SESSION['vnaam'] = "$vnaam";
+      $_SESSION['fnaam'] = "$fnaam";
+      $_SESSION['regid'] = $rowLogin['registratieID']; 
+
+      $_SESSION['weergave'] = "uitloggen";
+    }
+    else
+    {
+      // wachtwoord is fout
+      $loginFout = "Wachtwoord onjuist!";
+    }
+  }
+
+}
+
+
 ?>
--- Databanken/fietsgraveer2025/inc_aside.php
+++ Databanken/fietsgraveer2025/inc_aside.php
@@ -1,14 +1,21 @@
 <form name="frmLogin" id="frmLogin" method="post">
+  <?php
 
-<h4>Log hier op de website in</h4>
-<input type="text" name="txtVnaam" placeholder="Typ je voornaam in" class='inputsmall'>
-<input type="text" name="txtFnaam" placeholder="Typ je familienaam in" class='inputsmall'>
-<input type="password" name="txtWW" placeholder="Typ je wachtwoord in" class='inputsmall'>
-<p><input type="submit" name="btnLogin" value="Login"></p>
-
-<input type="submit" name="btnLogout" value="Logout XXX YYY">	
- 
-<p class='alert'>FEEDBACK/FOUTMELDINGEN</p>
+  if($_SESSION['weergave'] == 'inloggen')
+  {
+    echo '<h4>Log hier op de website in</h4>
+    <input type="text" name="txtVnaam" placeholder="Typ je voornaam in" class=-"inputsmall">
+    <input type="text" name="txtFnaam" placeholder="Typ je familienaam in" class="inputsmall">
+    <input type="password" name="txtWW" placeholder="Typ je wachtwoord in" class="inputsmall">
+    <p><input type="submit" name="btnLogin" value="Login"></p>';
+  }
+  else 
+  {
+    echo '<input type="submit" name="btnLogout" value="Logout $_SESSION["vnaam"] $_SESSION["fnaam"]">';	
+  }
+  
+  echo "<p class='alert'>$loginFout</p>";
+  ?>
 </form>
 
 <img src='images/fietsster.jpg' class='img-fluid'>
