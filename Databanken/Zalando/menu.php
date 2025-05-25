7af2af065a918c3150f2e7a3a97fd03336d24ad3
Daan Hautekeete <Hautekeete.Daan@gmail.com>
2025-05-25 15:59:28 +0200

    Zalando: toevoegen navigatie

--- Databanken/Zalando/menu.php
+++ Databanken/Zalando/menu.php
@@ -1,3 +1,17 @@
+<?php
+
+  // Sql statement maken om alle webpagina's op te halen
+  $sql = "Select * from tblmenu";
+  $result = $db->query($sql);
+
+  while($resultrow = $result->fetch_assoc()) {
+    $menutekst = $resultrow["menutekst"];
+    $menulink = $resultrow["menulink"];
+
+    $vulnavigatie .= "<li><a href='$menulink'>$menutekst</a></li>";
+  }
+?>
+
 <ul>
-<li><a href='XXX'>YYY</a></li>
+ <?= $vulnavigatie;?>
 </ul>
